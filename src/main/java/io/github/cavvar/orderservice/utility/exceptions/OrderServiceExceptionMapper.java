49515b80409b02c423e561ea4608388c012c7ee1
hm-angelett <d.angelett@hm.edu>
2021-06-05 18:45:57 +0200

    Fixed ExceptionMapper

--- src/main/java/io/github/cavvar/orderservice/utility/exceptions/OrderServiceExceptionMapper.java
+++ src/main/java/io/github/cavvar/orderservice/utility/exceptions/OrderServiceExceptionMapper.java
@@ -13,17 +13,15 @@ public class OrderServiceExceptionMapper implements ExceptionMapper<Throwable> {
     public Response toResponse(Throwable throwable) {
         final Response.ResponseBuilder responseBuilder = Response.status(Response.Status.NOT_FOUND).type(MediaType.TEXT_PLAIN_TYPE);
 
-        final Throwable throwableCause = throwable.getCause();
-
-        if (throwableCause instanceof NullPointerException) {
+        if (throwable instanceof NullPointerException) {
             responseBuilder.entity(String.format("Order or Item was not found. %s", throwable.getMessage()));
-        } else if (throwableCause instanceof NoSuchElementException) {
+        } else if (throwable instanceof NoSuchElementException) {
             responseBuilder.entity(String.format("Item was not found. %s", throwable.getMessage()));
-        } else if (throwableCause instanceof IllegalArgumentException) {
+        } else if (throwable instanceof IllegalArgumentException) {
             responseBuilder.entity(String.format("Order was not found. %s", throwable.getMessage()));
-        } else if (throwableCause instanceof IllegalStateException) {
+        } else if (throwable instanceof IllegalStateException) {
             responseBuilder.status(Response.Status.INTERNAL_SERVER_ERROR).entity(throwable.getMessage());
-        } else if (throwableCause instanceof BadRequestException) {
+        } else if (throwable instanceof BadRequestException) {
             responseBuilder.status(Response.Status.BAD_REQUEST).entity(String.format("Invalid Input. %s", throwable.getMessage()));
         }
 
